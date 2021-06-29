e1a24730c24eecc7eb870ef61287092ac72cfcaf
Quinn1876 <qhodges0442@gmail.com>
2021-06-28 20:50:08 -0400

    Add redirect for recruitment

---
+++ pages/recruitment.tsx
@@ -0,0 +1,2 @@
+import redirect from 'nextjs-redirect';
+export default redirect('https://www.notion.so/waterloop/Canadian-Hyperloop-Conference-Members-Recruitment-Page-10326bb7fe834e23a6a412ea04c88149');
