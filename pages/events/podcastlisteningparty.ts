eca530ba3640d049367423470423db361ac52976
Quinn1876 <qhodges0442@gmail.com>
2021-08-19 15:00:47 -0600

    listening event redirect link added

---
+++ pages/events/podcastlisteningparty.ts
@@ -0,0 +1,2 @@
+import redirect from 'nextjs-redirect';
+export default redirect('https://teams.microsoft.com/l/meetup-join/19%3ameeting_YTcyNDFiNjMtODIwMC00MzM1LWI2MDE[…]2c%22Oid%22%3a%22927eb1a3-f8d9-4339-8b3b-deb84336d106%22%7d');
