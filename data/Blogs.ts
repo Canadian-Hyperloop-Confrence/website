4e9dbf6d3435c84fd9f4f1942a3f5735adbdd660
Quinn1876 <qhodges0442@gmail.com>
2021-09-20 20:39:27 -0400

    Added Hyperloop 101 aka blog 3 to data

--- data/Blogs.ts
+++ data/Blogs.ts
@@ -18,6 +18,16 @@ const blogs: BlogCardData[] =  [
     },
     imageSrc: '/blogs/hyperloop-tt-blog2.png',
     link: 'https://waterloop.notion.site/CHC-Spotlight-Hyperloop-Transportation-Technologies-HyperloopTT-11e0caa9a70e438d9d0dd35c61872cdb'
+  },
+  {
+    title: 'Hyperloop 101',
+    date: {
+      day: 15,
+      month: 9,
+      year: 2021,
+    },
+    imageSrc: '/blogs/hyperloop101.jpg',
+    link: 'https://waterloop.notion.site/Hyperloop-101-5c9d0cbccc0940f0b38cd7188d8d19b7'
   }
 ]
 
