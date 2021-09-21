a35df1b7a2e2ae59199a5082e404a00649d3bc8a
Quinn1876 <qhodges0442@gmail.com>
2021-09-20 21:06:49 -0400

    Added CHC Virtual Showcase Announcement Video to Updates

--- data/Videos.ts
+++ data/Videos.ts
@@ -8,7 +8,18 @@ const Videos: BlogCardData[] =  [
     imageSrc: '/videos/world-wide-eng-podcast.png',
     link: 'https://www.youtube.com/watch?v=twxHAWdaLrY',
     title: 'CHCxWorldwide Engineering Podcast Listening Event with Guest Zak Smithe from HyperMap - Aug 19, 2021'
-  }
+  },
+  {
+    date: {
+      day: 1,
+      month: 9,
+      year: 2021,
+    },
+    imageSrc: '/videos/CHC virtual showcase announcement.jpg',
+    link: 'https://www.youtube.com/watch?v=Mzhu5rt8oiY&t=2s',
+    title: 'CHC Virtual Showcase Announcement'
+  },
+
 ];
 
 export default Videos;
