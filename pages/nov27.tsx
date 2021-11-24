21637ce7df0148124b85e373233eb142b9b53867
Quinn <39351374+Quinn1876@users.noreply.github.com>
2021-11-23 20:25:11 -0500

    Adding Redirect Links for 2021 November Virtual Showcase

---
+++ pages/nov27.tsx
@@ -0,0 +1,3 @@
+import redirect from 'nextjs-redirect';
+
+export default redirect('https://teams.microsoft.com/l/meetup-join/19%3ameeting_YzIzYmFlN2MtMTMwYi00NzMyLWFlODktZjBiNTY1MDg0NWY0%40thread.v2/0?context=%7b%22Tid%22%3a%22723a5a87-f39a-4a22-9247-3fc240c01396%22%2c%22Oid%22%3a%2210003cc6-38cf-4e39-9f61-1c1523a3fcf9%22%7d');
---
+++ pages/nov28registration.tsx
@@ -0,0 +1,3 @@
+import redirect from 'nextjs-redirect';
+
+export default redirect('https://teams.microsoft.com/registration/h1o6cprzIkqSRz_CQMATlg,7h_HNwV7YkWfZJQSWlgb0g,MLD0hVqK30yW7S83eaG46g,QcQ2JYChDUyjVPczyJYALg,FbvaRbBnrEGIBjopfF2LDA,yvTkt1RuqE6LuU3XG1gxQw?mode=read&tenantId=723a5a87-f39a-4a22-9247-3fc240c01396');
