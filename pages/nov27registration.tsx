74645fdeb75f6982e20b47ab4c88a4b45b3921cf
Quinn1876 <qhodges0442@gmail.com>
2021-12-06 03:38:01 -0500

    Removed Virtual Showcase links for nov27 / nov 28

--- pages/nov27registration.tsx
+++
@@ -1,3 +0,0 @@
-import redirect from 'nextjs-redirect';
-
-export default redirect('https://teams.microsoft.com/registration/h1o6cprzIkqSRz_CQMATlg,7h_HNwV7YkWfZJQSWlgb0g,MLD0hVqK30yW7S83eaG46g,_ZyEMWMFKUaIXnW3pm3lBw,YbGELyy020isOPjbueYIug,KvGPx9DL6kqFzgc1gPH5mw?mode=read&tenantId=723a5a87-f39a-4a22-9247-3fc240c01396');
