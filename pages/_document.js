307aa91a27fa1c467a51f015720a6d1071839b25
Quinn <39351374+Quinn1876@users.noreply.github.com>
2022-07-22 15:43:48 -0400

    update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -37,16 +37,14 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;900&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet"/>
       {/*<!-- Hotjar Tracking Code for https://www.cahyperloop.ca/ -->*/}
-          <script>
-            (function(h,o,t,j,a,r){
+      <script dangerourslySetInnerHtml={`(function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3071367,hjsv:6};
+                h._hjSettings={ hjid:3071367,hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}/>
         </Head>
         <body>
           <Main />
