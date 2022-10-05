3dd7843c927a24f4b59d8b7b98a5a689e91502b2
Quinn Hodges <qhodges0442@gmail.com>
2022-10-05 01:31:17 -0400

    Added 2022 Virtual Showcase tab to website

---
+++ data/files.ts
@@ -0,0 +1,14 @@
+export default {
+    "virtual-showcase-package": [
+        {
+            version: "1.0",
+            src: "/documents/Virtual_Showcase_Package.pdf"
+        },
+    ],
+    "virtual-showcase-application-form": [
+        {
+            version: "1.0",
+            src: "/documents/VIRTUAL_SHOWCASE_APPLICATION_DOCUMENT_TEMPLATE__.docx"
+        },
+    ]
+}
